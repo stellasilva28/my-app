ec756f46ec04d41563e830fbb435bd70a075f4a2
stellasilva28 <stella.nascimento2004@gmail.com>
2025-05-13 11:40:59 -0300

    Add files via upload

---
+++ my-App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text> Stella Nascimento</Text>
+      <Image source={require('./assets/stella.jpeg')} styles={styles.image} />
+      <Text style={styles.description}>
+        sou estudantes de analise e desenvolvimento de sistemas e trabalho com fotografia.
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    marginBottom: 20,
+  },
+});
